1bc72bd0aa6b5555bd9ad5ef11e254803b16f37d
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2025-04-13 11:41:01 -0700

    :construction: Work In Progress

--- src/lib/components/blog/BlogEditor.svelte
+++ src/lib/components/blog/BlogEditor.svelte
@@ -3,7 +3,7 @@
 	import type { Delta } from "quill/core";
 	import "quill/dist/quill.snow.css";
 	import { onMount } from "svelte";
-	import { blogOutput, titleToSlug } from "$lib/components/blog/blogOutput.svelte";
+	import { blogOutput, updateSlug, authorsRegistered } from "$lib/components/blog/blogOutput.svelte";
 
 	onMount(() => {
 		let quill = new Quill("#editor", {
@@ -32,17 +32,9 @@
 		});
 	});
 
-	let title = blogOutput.title;
-	let subtitle = $state("");
-	let postType = $state("");
-	let slug = $derived(titleToSlug(title));
-	let date: Date = $state(new Date());
-	let tags: string[];
-	let author = "";
-
 	let authorImage = ""; // not 100% sure of type, have to play with it
 	let authorTitle = ""; // eg. Owner - Pixel Mountain Creative
-	let cardImage: FileList | null | undefined = $state(); //I feel like this should ultimately have null and undefined removed
+	let cardImage: FileList | null | undefined = $state(); // I feel like this should ultimately have null and undefined removed
 	let cardImageUpload: any = $state();
 	let heroImage: FileList | null | undefined = $state();
 	let heroImageUpload: any = $state();
@@ -55,58 +47,56 @@
 	// After that, update the blogOutput object and send to the server
 
 	// <form method="post" action="https://api.cloudinary.com/v1_1/dvdwz9dpc/image/upload" enctype="multipart/form-data">
-
-	$effect(() => {
-		console.log(cardImageUpload);
-		console.log(new Date());
-	});
-
-	/*
-            "cardImage": "/assets/images/blogs/treehugger-hare-scramble-card.jpg",
-            "heroImage": "/assets/images/blogs/treehugger-hare-scramble-hero.jpg",
-            "tags": ["Digital Media"],
-            "author": "Jacob Druery",
-            "authorImage": "/assets/images/jacob-snowboarding-circle.png",
-            "authorTitle": "Owner - Pixel Mountain Creative",
-            "link": ""
-    */
 </script>
 
-<section>
-	<div class="blog-inputs">
-		<label
-			>Title:
-			<input name="title" type="text" bind:value={blogOutput.title} placeholder="Blog post title here..." required />
-		</label>
-		<label
-			>Subtitle:
-			<input name="subtitle" type="text" bind:value={blogOutput.subtitle} placeholder="Blog post subtitle here..." />
-		</label>
-		<label
-			>Author:
-			<select name="author" bind:value={blogOutput.author}>
-				<option value="Jacob Druery">Jacob Druery</option>
-				<option value="Guest Author">Guest Author</option>
-			</select>
-		</label>
-		<label
-			>Post Type:
-			<select bind:value={blogOutput.postType}>
-				<option value="blog">Blog</option>
-				<option value="project">Project</option>
-			</select>
-		</label>
-		<label
-			>Card Image:
-			<input type="file" bind:value={cardImageUpload} bind:files={cardImage} accept="image/jpeg" />
-		</label>
-	</div>
-	<div class="quill-container">
-		<div id="editor"></div>
-	</div>
-</section>
+<div class="dev-columns">
+	<section class="blog-editor">
+		<div class="blog-inputs">
+			<label
+				>Title:
+				<input name="title" type="text" oninput={updateSlug} bind:value={blogOutput.title} placeholder="Blog post title here..." required />
+			</label>
+			<p>Slug: {blogOutput.slug}</p>
+			<label
+				>Subtitle:
+				<input name="subtitle" type="text" bind:value={blogOutput.subtitle} placeholder="Blog post subtitle here..." />
+			</label>
+			<label
+				>Author:
+				<select name="author" bind:value={blogOutput.author}>
+					{#each authorsRegistered as author, i}
+						<option value={author}>
+							{author}
+						</option>
+					{/each}
+				</select>
+			</label>
+			<label
+				>Card Image:
+				<input type="file" bind:value={cardImageUpload} bind:files={cardImage} accept="image/jpeg" />
+			</label>
+		</div>
+		<div class="quill-container">
+			<div id="editor"></div>
+		</div>
+	</section>
+	<section class="blog-object-output">
+		<h2>Blog Output</h2>
+		<pre>{JSON.stringify(blogOutput, null, 2)}</pre>
+	</section>
+</div>
 
 <style>
+	/* Dev */
+
+	.dev-columns {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		grid-gap: 2rem;
+	}
+
+	/* ----------------------- */
+
 	#editor {
 		min-height: calc(300px);
 	}
--- src/lib/components/blog/blogOutput.svelte.ts
+++ src/lib/components/blog/blogOutput.svelte.ts
@@ -1,24 +1,40 @@
 import { Delta } from "quill/core";
 
+export const authorsRegistered = ["Jacob Druery", "Michelle Coish"];
+
 export let blogOutput = $state({
-    title: "",
-    subtitle: "",
-    postType: "blog" as BlogPostType,
-    slug: "",
-    author: "",
-    date: new Date(),
-    html: "",
-    delta: new Delta()
+	title: "",
+	subtitle: "",
+	slug: "",
+	author: authorsRegistered[0],
+	date: new Date(),
+	html: "",
+	delta: new Delta(),
+	postState: "draft" as BlogPostState
+});
 
-})
-
-type BlogPostType = "project" | "blog";
+type BlogPostState = "posted" | "draft" | "deleted";
 
 export function titleToSlug(title: string) {
-    if (!title) return;
-    return title
-        .trim() // Remove spaces at the beginning and end of the string
-        .toLowerCase()
-        .replace(/ /g, "-")
-        .replace(/[^\w-]+/g, "");
+	if (!title) return "";
+	return title
+		.trim() // Remove spaces at the beginning and end of the string
+		.toLowerCase()
+		.replace(/ /g, "-")
+		.replace(/[^\w-]+/g, "");
+}
+
+// TODO: Ensure when loading a draft, the slug is set to the correct value
+export function updateSlug() {
+	// Only update the slug if the post is in draft state
+	// This allows for the slug to be set when the post is first created
+	// and not changed when the post is published
+	if (blogOutput.postState === "draft") {
+		// Update the slug when the title changes
+		if (blogOutput.title) {
+			blogOutput.slug = titleToSlug(blogOutput.title);
+		} else {
+			blogOutput.slug = "";
+		}
+	}
 }
