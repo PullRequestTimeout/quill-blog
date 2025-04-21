3652d8caec88fff5ce716e9e336e636484f56291
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2025-04-20 21:09:06 -0700

    :construction: Refining admin page

--- src/routes/blog/admin/+page.svelte
+++ src/routes/blog/admin/+page.svelte
@@ -1,16 +1,40 @@
 <script lang="ts">
-	import { uiStore } from "$lib/stores/uiStore.svelte";
+	// Imports
 	import LoginForm from "$lib/components/LoginForm.svelte";
-
-	function handleLogout(event: Event) {
-		event.preventDefault();
-		console.log("Logging out...");
-	}
+	import AdminPanel from "$lib/components/AdminPanel.svelte";
+	import { uiStore } from "$lib/stores/uiStore.svelte";
+	import { authHandlers } from "$lib/firebase/auth";
 </script>
 
-{#if !uiStore.loggedIn}
-	<LoginForm />
-{:else if uiStore.loggedIn}
-	<div>Admin Panel</div>
-	<button onclick={handleLogout}>Log Out</button>
-{/if}
+<main>
+	<a class="button-link" href="/blog">Back</a>
+	{#if !uiStore.loggedIn}
+		<LoginForm />
+	{:else if uiStore.loggedIn}
+		<AdminPanel />
+		<button class="logout button button-primary" onclick={() => authHandlers.logout()}>Log Out</button>
+	{/if}
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		padding-top: 5rem;
+	}
+
+	a.button-link,
+	button.logout {
+		position: fixed;
+		top: 1rem;
+	}
+
+	button.logout {
+		right: 1rem;
+	}
+
+	a.button-link {
+		left: 1rem;
+	}
+</style>
