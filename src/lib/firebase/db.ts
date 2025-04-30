0e3515a095124cbb6b32d00eb7a7b5593aada1cc
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2025-04-30 09:23:16 -0700

    :construction: Further progress on DB functions

--- src/lib/firebase/db.ts
+++ src/lib/firebase/db.ts
@@ -18,7 +18,6 @@ import { doc, getDoc, setDoc, updateDoc, deleteDoc, collection, getDocs } from "
 import { handleAlertMessage } from "$lib/stores/uiStore.svelte";
 
 // Blog dependencies
-import { blogOutput } from "$lib/components/blog/blogOutput.svelte";
 import type { BlogPost } from "$lib/components/blog/blogOutput.svelte";
 
 export const databaseHandlers = {
@@ -42,12 +41,45 @@ export const databaseHandlers = {
 			if (!post.slug) {
 				throw new Error("Post must have a slug.");
 			}
+
 			const newPostRef = doc(db, "posts", post.slug);
-			await setDoc(newPostRef, post);
+			// Serialize the delta field if it exists
+			const serializedPost = {
+				...post,
+				delta: JSON.parse(JSON.stringify(post.delta))
+			};
+
+			await setDoc(newPostRef, serializedPost);
 			handleAlertMessage("Blog post created successfully.");
 		} catch (error) {
 			console.error("Error creating blog post: ", error);
 			handleAlertMessage("An error occurred trying to create the blog post.");
+		}
+	},
+
+	restoreBlogPost: async (post: BlogPost) => {
+		try {
+			if (!post.slug) {
+				throw new Error("Post must have a slug.");
+			}
+
+			// Check if the post exists in the database
+			const postRef = doc(db, "posts", post.slug);
+			const postSnapshot = await getDoc(postRef);
+
+			// If it does, and the post is not already in draft or published state, update it to draft state
+			if (!postSnapshot.exists()) {
+				handleAlertMessage("Blog post not found.");
+			} else if (post.postState === "draft" || post.postState === "published") {
+				handleAlertMessage("Blog post is already in draft or published state.");
+			} else {
+				const updatedPost = { ...post, postState: "draft" };
+				await updateDoc(postRef, updatedPost);
+				handleAlertMessage("Blog post restored successfully.");
+			}
+		} catch (error) {
+			console.error("Error restoring blog post: ", error);
+			handleAlertMessage("An error occurred trying to restore the blog post.");
 		}
 	}
 };
