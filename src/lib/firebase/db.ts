343306e5f096404de3a903e3d7647532c102ec7b
Jacob Druery <95809005+PullRequestTimeout@users.noreply.github.com>
2025-05-10 06:31:06 -0700

    :construction:  More DB functions

--- src/lib/firebase/db.ts
+++ src/lib/firebase/db.ts
@@ -48,15 +48,23 @@ export const databaseHandlers = {
 				...post,
 				delta: JSON.parse(JSON.stringify(post.delta))
 			};
-
-			await setDoc(newPostRef, serializedPost);
-			handleAlertMessage("Blog post created successfully.");
+			// Check if the post already exists in the database
+			const postSnapshot = await getDoc(newPostRef);
+			if (postSnapshot.exists()) {
+				handleAlertMessage("Blog post with this slug already exists.");
+				return;
+			} else if (!postSnapshot.exists()) {
+				await setDoc(newPostRef, serializedPost);
+				handleAlertMessage("Blog post created successfully.");
+			}
 		} catch (error) {
 			console.error("Error creating blog post: ", error);
 			handleAlertMessage("An error occurred trying to create the blog post.");
 		}
 	},
 
+	// Move a blog post from the deleted state to the draft state
+	// This is used when a user wants to restore a blog post that was previously deleted
 	restoreBlogPost: async (post: BlogPost) => {
 		try {
 			if (!post.slug) {
@@ -80,6 +88,77 @@ export const databaseHandlers = {
 		} catch (error) {
 			console.error("Error restoring blog post: ", error);
 			handleAlertMessage("An error occurred trying to restore the blog post.");
+		}
+	},
+
+	deleteBlogPost: async (post: BlogPost) => {
+		try {
+			if (!post.slug) {
+				throw new Error("Post must have a slug.");
+			}
+			// Check if the post exists in the database
+			const postRef = doc(db, "posts", post.slug);
+			const postSnapshot = await getDoc(postRef);
+
+			if (!postSnapshot.exists()) {
+				handleAlertMessage("Blog post not found.");
+			} else if (post.postState === "deleted") {
+				handleAlertMessage("Blog post has already been deleted.");
+			} else if (postSnapshot.exists() && (post.postState === "draft" || post.postState === "published")) {
+				const updatedPost = { ...post, postState: "deleted" };
+				await updateDoc(postRef, updatedPost);
+				handleAlertMessage("Blog post deleted.");
+			}
+		} catch (error) {
+			console.error("Error deleting blog post: ", error);
+			handleAlertMessage("An error occurred trying to delete the blog post.");
+		}
+	},
+
+	// get all tags to populate the tag selector component
+	getAllTags: async () => {
+		try {
+			const tagsSnapshot = await getDoc(doc(db, "tags", "tags"));
+			let tags: any = [];
+			if (!tagsSnapshot.exists()) {
+				// If the tags document does not exist, create one with an empty array and return an empty array
+				await setDoc(doc(db, "tags", "tags"), { tags: [] });
+				return [];
+			} else if (tagsSnapshot.data()) {
+				tags = tagsSnapshot.data().tags;
+				return tags;
+			}
+		} catch (error) {
+			console.error("Error getting tags: ", error);
+			handleAlertMessage("An error occurred trying to retrieve blog tags.");
+		}
+	},
+
+	// Add a new tag to the tags collection
+	addNewTag: async (tag: string) => {
+		try {
+			const tagsRef = doc(db, "tags", "tags");
+			const tagsSnapshot = await getDoc(tagsRef);
+
+			// Check if the tags document exists, if not, create it
+			// If it exists, only add the new tag if it doesn't already exist
+			if (!tagsSnapshot.exists()) {
+				// If the tags document does not exist, create one with an empty array
+				await setDoc(tagsRef, { tags: [tag] });
+				handleAlertMessage("Tag added successfully.");
+			} else {
+				const currentTags = tagsSnapshot.data().tags || [];
+				if (!currentTags.includes(tag)) {
+					currentTags.push(tag);
+					await updateDoc(tagsRef, { tags: currentTags });
+					handleAlertMessage("Tag added successfully.");
+				} else if (currentTags.includes(tag)) {
+					handleAlertMessage("Tag already exists.");
+				}
+			}
+		} catch (error) {
+			console.error("Error adding tag: ", error);
+			handleAlertMessage("An error occurred trying to add the tag.");
 		}
 	}
 };
